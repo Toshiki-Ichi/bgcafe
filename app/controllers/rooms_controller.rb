d5d3685285606b2aea8b4f7f1baa94838a85d37b
u-nami <chikayym@gmail.com>
2024-10-01 17:53:58 +0900

    トップ画面のレイアウトとリンクの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,36 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:edit, :show, :update]
+
   def index
-    @rooms = Room.includes(:user)
+    @rooms = Room.includes(user_rooms: :user) 
   end
 
   def new
+    @room =Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+end
+
+def destroy
+end
+
+def show
+end
+
+private
+
+  def room_params
+   params.require(:room).permit(:room_name,:orner,:image_rooms) 
+  end
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 end
