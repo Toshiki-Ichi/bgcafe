9795b163ea37fcfba3b80f3af80770e0a1a465e0
u-nami <chikayym@gmail.com>
2024-10-02 16:01:48 +0900

    テーブル設定の見直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_30_083421) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_02_070011) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_30_083421) do
     t.string "orner"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "contact"
   end
 
   create_table "user_rooms", charset: "utf8mb3", force: :cascade do |t|
@@ -63,8 +64,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_30_083421) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "nickname"
+    t.integer "career_id"
+    t.string "likes"
+    t.string "weakness"
+    t.string "sns"
+    t.text "note"
+    t.integer "join1"
+    t.integer "join2"
+    t.integer "join3"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
