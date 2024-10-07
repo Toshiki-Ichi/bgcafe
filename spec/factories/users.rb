ed731923de4150b1c7b7357647611bd5f4756597
u-nami <chikayym@gmail.com>
2024-10-07 12:49:43 +0900

    roomモデルの単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,6 +9,8 @@ FactoryBot.define do
     weakness              { Faker::Lorem.sentence }
     sns                   { Faker::Internet.url }
     note                  { Faker::Lorem.paragraphs(number: 3).join(" ") }
-
+    after(:build) do |user|
+      user.my_image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
