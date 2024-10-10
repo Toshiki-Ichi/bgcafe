a216a1ef8011a802ab517a9330d684bbac57d09e
u-nami <chikayym@gmail.com>
2024-10-10 23:10:24 +0900

    日付ごとのメンバーリストを表示する

--- app/controllers/groupschedules_controller.rb
+++ app/controllers/groupschedules_controller.rb
@@ -1,8 +1,16 @@
 class GroupschedulesController < ApplicationController
-def new
+def index
+	@user = current_user
+	@room = Room.find(params[:room_id])
+end
+
+	def new
 	@user = current_user
 	@room = Room.find(params[:room_id])
 
+	@ownplans = Ownplan.where(room_id: @room.id, target_week: nil)
+	@targetweek = Ownplan.where(room_id: @room.id).where.not(target_week: nil)
+	@target_week_dates = @targetweek.pluck(:target_week).map { |date| date.to_date}
 end
 
 end
