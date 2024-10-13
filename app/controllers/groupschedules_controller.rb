605d05fbe096c88d292fb8d8dfa9a804259b431f
u-nami <chikayym@gmail.com>
2024-10-13 18:56:26 +0900

    組み合わせ数の制限　と　チェックボックスの表示

--- app/controllers/groupschedules_controller.rb
+++ app/controllers/groupschedules_controller.rb
@@ -11,6 +11,7 @@ class GroupschedulesController < ApplicationController
     @ownplans = Ownplan.where(room_id: @room.id, target_week: nil)
     @targetweek = Ownplan.where(room_id: @room.id).where.not(target_week: nil)
     @target_week_dates = @targetweek.pluck(:target_week).map { |date| date.to_date }
+		@groupschedule = Groupschedule.new
   end
 
   def create
