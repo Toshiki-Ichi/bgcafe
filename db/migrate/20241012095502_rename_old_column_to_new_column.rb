61ff5330a70e6184d9b2a12b01aace550ebd5dfa
u-nami <chikayym@gmail.com>
2024-10-23 17:03:34 +0900

    Revert "マイグレーションファイルの削除2"
    This reverts commit da2d79b5df5832bc700428734635d9923b575788.

---
+++ db/migrate/20241012095502_rename_old_column_to_new_column.rb
@@ -0,0 +1,16 @@
+class RenameOldColumnToNewColumn < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :groupschedules, :group1, :group1_daytime
+    rename_column :groupschedules, :group2, :group2_daytime
+    rename_column :groupschedules, :group3, :group3_daytime
+    rename_column :groupschedules, :group4, :group1_20pm
+    rename_column :groupschedules, :group5, :group2_20pm
+    rename_column :groupschedules, :group6, :group3_20pm
+    rename_column :groupschedules, :group7, :group1_21pm
+    rename_column :groupschedules, :group8, :group2_21pm
+    rename_column :groupschedules, :group9, :group3_21pm
+    rename_column :groupschedules, :group10, :group1_22pm
+    add_column :Groupschedules, :group2_22pm, :string
+    add_column :Groupschedules, :group3_22pm, :string
+  end
+end
