54b38b4836639fd3f6c0cccd65ec6fb6f95427c4
u-nami <chikayym@gmail.com>
2024-10-24 16:44:44 +0900

    スケジュールにリンクを設定

--- db/migrate/20241024021524_add_group22pm_to_groupschedules.rb
+++
@@ -1,6 +0,0 @@
-class AddGroup22pmToGroupschedules < ActiveRecord::Migration[7.0]
-  def change
-    add_column :groupschedules, :group2_22pm, :string
-    add_column :groupschedules, :group3_22pm, :string
-  end
-end
